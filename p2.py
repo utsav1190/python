72558686b28506e16595ee0d893c3123e8a8031d
UTSAV MORADIYA <utsavmoradiya48@gmail.com>
2026-01-08 11:18:58 +0530

    Create p2.py

---
+++ p2.py
@@ -0,0 +1,16 @@
+
+#WAP TO CREATE VARAIBLE & STORE VALUE FROM USER AND DISPLAY DETAILS 
+#create a program to accept below detail from user and display it 
+#college Name branch semester hobbies
+#NAME=KRISHNADEEPSINH
+#DATE=3/1/26
+
+college_name=input("ENTER COLLEGE NAME = ")             #take input from user 
+branch=input("ENTER BRANCH =")
+sem=input("ENTER SEMESTER =")
+hobbies=input("ENTER YOUR HOBBIES =")
+
+print("CLG_NAME=",college_name)
+print("BRANCH=",branch)
+print("SEM=",sem)
+print("HOBBIES=",hobbies)
