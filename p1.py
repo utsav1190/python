2ec6095861b2a72778738c7a51412d8324a64013
UTSAV MORADIYA <utsavmoradiya48@gmail.com>
2026-01-08 11:18:24 +0530

    Add personal details display script in p1.py
    This script creates variables to store personal details and displays them.

---
+++ p1.py
@@ -0,0 +1,20 @@
+
+#WAP TO CREATE VARAIBLE & STORE VALUE AND DISPLAY DETAILS 
+#NAME=KRISHNADEEPSINH
+#DATE=3/1/26
+
+name="CHUDASAMA KRISHNADEEPSINH"           #VARIABLE DECLARE AND VALUE ASSIGN
+address="CHITRA "
+city="BHAVNAGAR"
+pincode=364004
+age=19
+weight=82
+gender="MALE"
+
+print(" NAME = ",name)
+print(" ADDRESS = ",address)
+print(" CITY = ",city)
+print(" PINCODE = ",pincode)
+print(" AGE = ",age)
+print(" WEIGHT = ",weight)
+print(" GENDER = ",gender)
